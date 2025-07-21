7139b28edf66c4d35e3cc5bc04115fcbd1c4f3d0
David Gamil <105387640+DavidG2Q@users.noreply.github.com>
2025-07-21 03:48:32 +0300

    Delete cart directory

--- cart/1.js
+++
@@ -1,9 +0,0 @@
-function showLoading(event, button) {
-    event.preventDefault(); // Prevent form submission
-  
-    button.innerHTML = "Processing Payment...";
-  
-    setTimeout(function() {
-      button.innerHTML = "Payment completed.";
-    }, 3000); // Change to the desired duration in milliseconds
-  }
